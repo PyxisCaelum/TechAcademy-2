ff46ea41951fe4915885dce15a391ad8e1fd100a
PyxisCaelum <felipealvesbatista28@gmail.com>
2024-06-11 23:00:10 -0300

    Arrumando o commit que eu acabei de fazer

--- Site Novo do Felipe/paginas/erro.php
+++ Site Novo do Felipe/paginas/erro.php
@@ -1,7 +1,8 @@
-<div class="container">
+<div class="erro404con">
     <p>
         <img src="imagens\404.png"
         alt="Erro 404"
-        title="Erro 404">
+        title="Erro 404"
+        class="erroimg">
     </p>
 </div>
--- paginas/erro.php
+++
@@ -1,8 +0,0 @@
-<div class="erro404con">
-    <p>
-        <img src="imagens\404.png"
-        alt="Erro 404"
-        title="Erro 404"
-        class="erroimg">
-    </p>
-</div>
