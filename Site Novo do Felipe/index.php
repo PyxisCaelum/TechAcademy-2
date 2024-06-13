87b577fc346977bd5765374f85748862a2a21c7b
PyxisCaelum <felipealvesbatista28@gmail.com>
2024-06-12 22:21:21 -0300

    Arrumando a página de erro e adicionando um carrossel

--- Site Novo do Felipe/index.php
+++ Site Novo do Felipe/index.php
@@ -5,12 +5,12 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-    <link rel="stylesheet" href="css/style.css">
-
     <link rel="shortcut icon" href="imagens\Logo.png">
     <link rel="stylesheet" href="css/bootstrap.min.css">
     <link rel="stylesheet" href="css/bootstrap-grid.css">
     <link rel="stylesheet" href="css/bootstrap-reboot.css">
+
+    <link rel="stylesheet" href="css/style.css">
 
     <title>Escapism</title>
 
